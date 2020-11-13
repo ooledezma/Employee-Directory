6225d7c0ea49778515db302a1ddde4edd32d0e30
ooledezma <ooledezma@gmail.com>
2020-11-12 17:04:35 -0800

    added header and wrapper component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>Click on each heading or use the search box to organize and narrow your results.</p>
+  </div>
+  );
+}
+
+export default Header;
